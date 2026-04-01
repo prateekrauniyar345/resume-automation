f5d4639df8b93165a5375269bc9c5777bdabc830
prateekrauniyar345 <raun9583@vandals.uidaho.edu>
2026-03-31 21:12:52 -0700

    added  default route

--- Backend/main.py
+++ Backend/main.py
@@ -1,12 +1,23 @@
 from flask import Flask
 from dotenv import load_dotenv
 import os
-
+from datetime import datetime
 # load environment variables from .env file
 load_dotenv()
 
 
 app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return {
+        "message" : "Welcome to RESUME TAILOR AGENT API",
+        "status" : "running", 
+        "code" : 200, 
+        "date" : datetime.now().isoformat()
+    } 
+
 
 
 if __name__ == "__main__":
